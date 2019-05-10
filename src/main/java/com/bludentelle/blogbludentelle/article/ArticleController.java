cdcd16948947a60b78fb6f136ddd141754493e8e
Pierre Lambert <pierre.lambert-prestataire@laposte.fr>
2019-05-10 14:54:01 +0200

    ajout CRUD articleController

--- src/main/java/com/bludentelle/blogbludentelle/article/ArticleController.java
+++ src/main/java/com/bludentelle/blogbludentelle/article/ArticleController.java
@@ -1,11 +1,11 @@
 package com.bludentelle.blogbludentelle.article;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.data.rest.webmvc.ResourceNotFoundException;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
 
+import javax.validation.Valid;
 import java.util.List;
 import java.util.Optional;
 
@@ -20,4 +20,28 @@ public class ArticleController {
         return arp.findAll();
     }
 
+    @PostMapping("/posts")
+    public ArticleModel createArticle(@Valid @RequestBody ArticleModel article) {
+        return arp.save(article);
+    }
+
+    @PutMapping("/posts/{id}")
+    public ArticleModel updateArticle(@PathVariable int articleId, @Valid @RequestBody ArticleModel articleRequest) {
+        return arp.findById(articleId).map(articleModel -> {
+            articleModel.setTitle(articleRequest.getTitle());
+            articleModel.setDescription(articleRequest.getDescription());
+            articleModel.setCategory(articleRequest.getCategory());
+            articleModel.setUser(articleRequest.getUser());
+            articleModel.setModifiedDatetime(articleRequest.getModifiedDatetime());
+            return arp.save(articleRequest);
+        }).orElseThrow(() -> new ResourceNotFoundException("ArticleId" + articleId + "not found"));
+    }
+
+    @DeleteMapping("/posts/{postId}")
+    public ResponseEntity<?> deletePost(@PathVariable int articleId) {
+        return arp.findById(articleId).map(post -> {
+            arp.delete(post);
+            return ResponseEntity.ok().build();
+        }).orElseThrow(() -> new ResourceNotFoundException("ArticleId " + articleId + " not found"));
+    }
 }
