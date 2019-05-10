f5404e2aba1134d8d71a75b009192ad726268596
pierrelambert41 <pierrelambert41@gmail.com>
2019-05-10 21:06:25 +0200

    fix mapping controller

--- src/main/java/com/bludentelle/blogbludentelle/article/ArticleController.java
+++ src/main/java/com/bludentelle/blogbludentelle/article/ArticleController.java
@@ -1,5 +1,6 @@
 package com.bludentelle.blogbludentelle.article;
 
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.rest.webmvc.ResourceNotFoundException;
 import org.springframework.http.ResponseEntity;
@@ -15,17 +16,19 @@ public class ArticleController {
     @Autowired
     ArticleRepository arp;
 
+    @CrossOrigin
     @GetMapping("/articles")
     public List<ArticleModel> findAll() {
         return arp.findAll();
     }
 
-    @PostMapping("/posts")
+
+    @PostMapping("/articles")
     public ArticleModel createArticle(@Valid @RequestBody ArticleModel article) {
         return arp.save(article);
     }
 
-    @PutMapping("/posts/{id}")
+    @PutMapping("/articles/{id}")
     public ArticleModel updateArticle(@PathVariable int articleId, @Valid @RequestBody ArticleModel articleRequest) {
         return arp.findById(articleId).map(articleModel -> {
             articleModel.setTitle(articleRequest.getTitle());
@@ -37,7 +40,7 @@ public class ArticleController {
         }).orElseThrow(() -> new ResourceNotFoundException("ArticleId" + articleId + "not found"));
     }
 
-    @DeleteMapping("/posts/{postId}")
+    @DeleteMapping("/articles/{articleId}")
     public ResponseEntity<?> deletePost(@PathVariable int articleId) {
         return arp.findById(articleId).map(post -> {
             arp.delete(post);
--- src/main/resources/query/bludentelle.sql
+++ src/main/resources/query/bludentelle.sql
@@ -1,12 +1,24 @@
 TRUNCATE TABLE article;
 INSERT INTO article
-(title, created_date, modified_date, description) VALUES
-("FRCYMB","2018-09-24","2018-09-24", "ceci est mon 1er paragraphe"),
-("FRCYMB","2018-09-24","2018-09-24", "ceci est mon 2er paragraphe");
+(title, created_at, modified_at, description, user_id, category_id) VALUES
+("FRCYMB","2018-09-24","2018-09-24", "ceci est mon 1er paragraphe", 1, 1),
+("FRCYMB","2018-09-24","2018-09-24", "ceci est mon 2er paragraphe", 2, 2);
 
 TRUNCATE TABLE user;
 INSERT INTO user
-(firstname, lastname) VALUES
-("Aurelie", "Lambert"),
-("Pierre", "Lambert");
+(firstname, lastname, email) VALUES
+("Aurelie", "Lambert", "lambertaurelie17@gmail.com"),
+("Pierre", "Lambert", "pierrelambert41@gmail.com");
+
+TRUNCATE TABLE category;
+INSERT INTO category
+(name) VALUES
+("Mariage"),
+("Voyage");
+
+TRUNCATE TABLE comment;
+INSERT INTO comment
+(author, content, email, created_at, article_id) VALUES
+("Toto", "ceci est mon commentaire 1", "toto@gmail.com", "2019-05-12", 1),
+("Toto2", "ceci est mon commentaire 2", "toto2@gmail.com", "2019-05-12", 2);
 
